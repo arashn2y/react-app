2ae2f94db4e7d2559c999485454e64695e803109
BrnDavide <brn.davide@gmail.com>
2024-03-25 08:54:20 +0100

    react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-function Header() {
+import { CiLogin as Login} from "react-icons/ci";
+function Header({title}) {
+ //console.log({title});
   return (
     <header className="flex justify-between items-center bg-green-400 h-16">
       <div className="w-full flex justify-end items-center">
-        <h1 className="text-4xl text-white text-center">Feedback UI</h1>
+        <h1 className="text-4xl text-white text-center">{title}</h1>
         <div className=" w-1/2 flex justify-end items-center">
-          <button className="bg-yellow-300 text-white py-2 px-4 rounded mr-3">Login</button>
+          <button className="text-4xl text-white py-2 px-4 rounded mr-3">
+            <Login />
+            </button>
+          
         </div>
       </div>
     </header>
